4c4fc14c1487dfa97a93a98ada02713852d375a2
Sven Meinhardt <sven@meinhardt.ai>
2025-11-04 13:56:07 +0000

    Extract constants into their own modules

---
+++ yfinance/screener/region_exchange_code_mapping.py
@@ -0,0 +1,120 @@
+REGION_EXCHANGE_CODE_MAPPING = {
+    "United Arab Emirates": {"Dubai": "DFM"},
+    "Argentina": {"Buenos Aires": "BUE"},
+    "Austria": {"Vienna": "VIE"},
+    "Australia": {"ASX": "ASC", "Cboe AU": "CXA"},
+    "Belgium": {"Brussels": "BRU"},
+    "Brazil": {"Sao Paulo": "SAO"},
+    "Canada": {
+        "Canadian Sec": "CNQ",
+        "_NEO": "NEO",
+        "Toronto": ["TOR", "TSI"],
+        "_VAN": "VAN",
+    },
+    "Switzerland": {"Swiss": "EBS"},
+    "Chile": {"Santiago": "SGO"},
+    "China": {"Shanghai": "SHH", "Shenzhen": "SHZ"},
+    "Colombia": {"_BVC": "BVC"},
+    "Czechia": {"Prague": "PRA"},
+    "Germany": {
+        "Berlin": "BER",
+        "Dusseldorf": "DUS",
+        "Frankfurt": "FRA",
+        "Hamburg": "HAM",
+        "Hanover": "HAN",
+        "XETRA": "GER",
+        "Munich": "MUN",
+        "Stuttgart": "STU",
+    },
+    "Denmark": {"Copenhagen": "CPH"},
+    "Estonia": {"Tallinn": "TAL"},
+    "Egypt": {"_CAI": "CAI"},
+    "Spain": {"Barcelona": "BAR", "Madrid": "MCE"},
+    "Finland": {"Helsinki": "HEL"},
+    "France": {"Paris": "PAR"},
+    "United Kingdom": {
+        "Cboe Canada": "AQS",
+        "_IOB": "IOB",
+        "_LSE": "LSE",
+        "Cboe UK": "CXE",
+    },
+    "Greece": {"Athens": "ATH"},
+    "Hong Kong": {"HKSE": "HKG"},
+    "Hungary": {"Budapest": "BUD"},
+    "Indonesia": {"Jakarta": "JKT"},
+    "Ireland": {"Irish": "ISE"},
+    "Israel": {"Tel Aviv": "TLV"},
+    "India": {"_BSE": "BSE", "_NSI": "NSI"},
+    "Iceland": {"Iceland": "ICE"},
+    "Italy": {"Milan": "MIL"},
+    "Japan": {
+        "Fukuoka": "FKA",
+        "Tokyo": "JPX",
+        "Sapporo": "SAP",
+        "Osaka": "OSA",
+    },
+    "South Korea": {"_KOE": "KOE", "_KSC": "KSC"},
+    "Kuwait": {"Kuwait": "KUW"},
+    "Sri Lanka": {"Colombo": "CSE"},
+    "Lithuania": {"Vilnius": "LIT"},
+    "Latvia": {"Riga": "RIS"},
+    "Mexico": {"Mexico": "MEX"},
+    "Malaysia": {"Kuala Lumpur": "KLS"},
+    "Netherlands": {"Amsterdam": "AMS"},
+    "Norway": {"_OSL": "OSL"},
+    "New Zealand": {"_NZE": "NZE"},
+    "Peru": {},
+    "Philippines": {"_PHP": "PHP", "_PHS": "PHS"},
+    "Pakistan": {
+        "Karachi": "KAR",
+    },
+    "Poland": {"Warsaw": "WSE"},
+    "Portugal": {"Lisbon": "LIS"},
+    "Qatar": {"_DOH": "DOH"},
+    "Romania": {"_BVB": "BVB"},
+    "Russia": {},
+    "Saudi Arabia": {"Saudi": "SAU"},
+    "Sweden": {"Stockholm": "STO"},
+    "Singapore": {"_SES": "SES"},
+    "Suriname": {},
+    "Thailand": {"Thailand": "SET"},
+    "Turkey": {"Istanbul": "IST"},
+    "Taiwan": {"Taiwan": "TAI", "Taipei Exchange": "TWO"},
+    "United States": {
+        "Nasdaq": "NAS",
+        "NYSE American": ["ASE", "ASQ"],
+        "Cboe US": "BTS",
+        "_CXI": "CXI",
+        "Nasdaq CM": "NCM",
+        "Nasdaq GM": "NGM",
+        "Nasdaq GS": "NMS",
+        "NYSE": "NYQ",
+        "OTC Markets EXMKT": "OEM",
+        "OTC Markets OTCQB": "OQB",
+        "OTC Markets OTCQX": "OQX",
+        "_PCX": "PCX",
+        "OTC Markets": "PNK",
+        "_YHD": "YHD",
+        "Chicago Options": "WCB",
+    },
+    "Venezuela": {"Caracas": "CCS"},
+    "Vietnam": {},
+    "South Africa": {"Johannesburg": "JNB"},
+}
+
+
+# Exchanges not yet mapped in REGION_EXCHANGE_MAPPING:
+# Cboe UK
+# Euronext -> ENX
+# EUREX
+# MCE
+# MCX
+# KOSDAQ
+# KSE
+# NSE
+# NZSE
+# OTC Markets Grey
+# SES
+# TSXV
+# HOSE
+# EGX
